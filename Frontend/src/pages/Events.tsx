492bbbac90ae6cba6ab7ce86a813291887e10b4a
sangram behera <sangrambeheraa007@gmail.com>
2025-11-29 14:20:28 +0530

    html and filter fix

--- Frontend/src/pages/Events.tsx
+++ Frontend/src/pages/Events.tsx
@@ -120,7 +120,7 @@ const Events = () => {
               </SelectContent>
             </Select>
 
-            <Select value={location} onValueChange={setLocation}>
+            {/* <Select value={location} onValueChange={setLocation}>
               <SelectTrigger>
                 <SelectValue placeholder='Location' />
               </SelectTrigger>
@@ -131,7 +131,7 @@ const Events = () => {
                   </SelectItem>
                 ))}
               </SelectContent>
-            </Select>
+            </Select> */}
 
             <Button onClick={handleApplyFilters}>Apply Filters</Button>
           </div>
